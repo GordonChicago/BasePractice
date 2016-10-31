49b7edcac80a77ed154c871afcc215e177d85773
yangyibao <yangyibao@yunfan.com>
2016-10-31 17:17:09 +0800

    圆角其他写法

--- app/src/main/java/com/basepractice/view/RoundImageView.java
+++ app/src/main/java/com/basepractice/view/RoundImageView.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
@@ -139,5 +141,36 @@ public class RoundImageView extends ImageView {
         super.onSizeChanged(w, h, oldw, oldh);
         if (mType == TYPE_ROUND_CORNER)
             mRoundRect = new RectF(0, 0, getWidth(), getHeight());
+    }
+
+    /**
+     * 类似的相关圆角方法
+     */
+    /**
+     * 获取圆角矩形图片方法
+     * @param bitmap
+     * @param roundPx,一般设置成14
+     * @return Bitmap
+     * @author caizhiming
+     */
+    private Bitmap getRoundBitmap(Bitmap bitmap, int roundPx) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        int x = bitmap.getWidth();
+
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
     }
 }
