aa2fafe8a438aeb52b6ea93e32e2378829f29767
724380361@qq.com <yangbaobao@yunfan.com>
2016-12-14 11:09:23 +0800

    提交

--- app/src/main/java/com/basepractice/ValueAnimator.java
+++ app/src/main/java/com/basepractice/ValueAnimator.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+
 import com.basepractice.util.ViewUtils;
 
 public class ValueAnimator extends Activity {
@@ -15,28 +16,30 @@ public class ValueAnimator extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.value_animator);
     }
-    public void testTranslationX(View view){
-        ObjectAnimator.ofFloat(view,"translationX",ViewUtils.dpToPx(this,300)).setDuration(2000).start();
+
+    public void testTranslationX(View view) {
+        ObjectAnimator.ofFloat(view, "translationX", ViewUtils.dpToPx(this, 300)).setDuration(2000).start();
     }
 
-    public void tanslationAnimation(View view){
+    public void tanslationAnimation(View view) {
         View txtview = findViewById(R.id.text1);
-        TranslateAnimation translateAnimation = new TranslateAnimation(100,100,0,100);
+        TranslateAnimation translateAnimation = new TranslateAnimation(100, 100, 0, 100);
         translateAnimation.setDuration(2000);
         translateAnimation.setFillAfter(true);
         txtview.startAnimation(translateAnimation);
     }
-    public void rotateAnimation(View view){
+
+    public void rotateAnimation(View view) {
         View txtview = findViewById(R.id.text1);
-        RotateAnimation rotateAnimation = new RotateAnimation(0,360,txtview.getWidth()/4,txtview.getHeight()/2);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, txtview.getWidth() / 4, txtview.getHeight() / 2);
         rotateAnimation.setDuration(2000);
         rotateAnimation.setFillAfter(true);
         txtview.startAnimation(rotateAnimation);
     }
 
-    public void scacleAnimation(View view){
+    public void scacleAnimation(View view) {
         View txtview = findViewById(R.id.text1);
-        ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,1);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1, 2, 1, 1);
         scaleAnimation.setDuration(3000);
         scaleAnimation.setFillAfter(true);
         txtview.startAnimation(scaleAnimation);
