377cf3d549131110f9f0a03ff0c68ab3c91664a6
724380361@qq.com <724380361@qq.com>
2016-12-31 20:25:41 +0800

    1

--- app/src/main/java/com/basepractice/ValueAnimator.java
+++ app/src/main/java/com/basepractice/ValueAnimator.java
@@ -2,14 +2,12 @@ package com.basepractice;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 
-import com.basepractice.dialog.ShootingModeChatActivity;
 import com.basepractice.util.ViewUtils;
 /**
  * Created by Administrator on 2016/10/9.
